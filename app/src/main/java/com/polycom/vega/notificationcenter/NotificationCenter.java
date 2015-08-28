7e84727b80b95438df7495b271b00bdcc285dc8e
Michael Cheng <XinwenCheng@Autumoon.com>
2015-08-28 14:55:39 +0800

    Source code archive.

--- app/src/main/java/com/polycom/vega/notificationcenter/NotificationCenter.java
+++ app/src/main/java/com/polycom/vega/notificationcenter/NotificationCenter.java
@@ -3,6 +3,10 @@ package com.polycom.vega.notificationcenter;
 import android.text.TextUtils;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by xwcheng on 8/28/2015.
@@ -14,26 +18,58 @@ public class NotificationCenter {
         return (instance == null ? instance = new NotificationCenter() : instance);
     }
 
-    private HashMap<String, String> notifications;
+    private HashMap<String, String> cyclicNotifications;
+    private HashMap<String, String> oneTimeNotifications;
 
     private NotificationCenter() {
-        notifications = new HashMap<String, String>();
+        cyclicNotifications = new HashMap<String, String>();
+        timer = new Timer("notificationTimer", true);
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                Iterator iterator = cyclicNotifications.entrySet().iterator();
+
+                while (iterator.hasNext()) {
+                    Map.Entry pair = (Map.Entry) iterator.next();
+                    String restPath = pair.getKey().toString();
+                    String messageName = pair.getValue().toString();
+
+                    // TODO: Request from rest.
+
+                    iterator.remove();
+                }
+            }
+        }, 0, 10000);
+
+        oneTimeNotifications = new HashMap<String, String>();
     }
 
-    public String registerNotification(String notificationRestPath) {
+    private Timer timer;
+
+    public String registerNotification(String notificationRestPath, boolean isCyclic) {
         if (TextUtils.isEmpty(notificationRestPath)) {
             return "";
         }
 
-        String messageName = notifications.get(notificationRestPath);
+        String messageName = "";
+
+        if (isCyclic) {
+            messageName = cyclicNotifications.get(notificationRestPath);
+        } else {
+            messageName = oneTimeNotifications.get(notificationRestPath);
+        }
 
         if (!TextUtils.isEmpty(messageName)) {
             return messageName;
         }
 
-        messageName = String.format("NotificationMessage%d", notifications.size());
+        messageName = String.format("NotificationMessage%d", cyclicNotifications.size());
 
-        notifications.put(notificationRestPath, messageName);
+        if (isCyclic) {
+            cyclicNotifications.put(notificationRestPath, messageName);
+        } else {
+            oneTimeNotifications.put(notificationRestPath, messageName);
+        }
 
         return messageName;
     }
