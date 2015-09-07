875ae7cf4e321876d0a4b90960a39526a1b03cb1
Michael Cheng <XinwenCheng@Autumoon.com>
2015-09-07 13:48:18 +0800

    Add local storage helper class.

--- app/src/main/java/com/polycom/vega/localstorage/LocalStorageHelper.java
+++ app/src/main/java/com/polycom/vega/localstorage/LocalStorageHelper.java
@@ -1,5 +1,8 @@
 package com.polycom.vega.localstorage;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 /**
  * Created by xwcheng on 9/7/2015.
  */
@@ -13,5 +16,29 @@ public class LocalStorageHelper {
     }
 
     private LocalStorageHelper() {
+    }
+
+    public String get(Context context, String key) throws Exception {
+        if (context == null) {
+            throw new Exception("Context is null.");
+        }
+
+        SharedPreferences sharedPreferences = context.getSharedPreferences(storageName, Context.MODE_PRIVATE);
+
+        return (sharedPreferences == null ? null : sharedPreferences.getString(key, null));
+    }
+
+    public void save(Context context, String key, String value) throws Exception {
+        if (context == null) {
+            throw new Exception("Context is null.");
+        }
+
+        try {
+            SharedPreferences.Editor editor = context.getSharedPreferences(storageName, Context.MODE_PRIVATE).edit();
+            editor.putString(key, value);
+            editor.commit();
+        } catch (Exception ex) {
+            throw ex;
+        }
     }
 }
