7352f3d3cacdb7d342765546dc1615f696ee6762
XinwenCheng <XinwenCheng@Autumoon.com>
2015-09-22 16:25:21 +0800

    Improved local storage.

--- app/src/main/java/com/polycom/vega/localstorage/LocalStorageHelper.java
+++ app/src/main/java/com/polycom/vega/localstorage/LocalStorageHelper.java
@@ -2,12 +2,14 @@ package com.polycom.vega.localstorage;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.text.TextUtils;
 
 /**
  * Created by xwcheng on 9/7/2015.
  */
 public class LocalStorageHelper {
     private static String storageName = "com.polycom.vega";
+    private static String languageKey = "language";
 
     private static LocalStorageHelper instance;
 
@@ -18,9 +20,27 @@ public class LocalStorageHelper {
     private LocalStorageHelper() {
     }
 
-    public String get(Context context, String key) throws Exception {
+    public String getLanguage(Context context) throws IllegalArgumentException {
         if (context == null) {
-            throw new Exception("Context is null.");
+            throw new IllegalArgumentException("Context is null.");
+        }
+
+        return get(context, languageKey);
+    }
+
+    public void saveLanguage(Context context, String language) throws IllegalArgumentException {
+        if (context == null) {
+            throw new IllegalArgumentException("Context is null.");
+        } else if (TextUtils.isEmpty(language)) {
+            throw new IllegalArgumentException("Language name is empty.");
+        }
+
+        save(context, languageKey, language);
+    }
+
+    public String get(Context context, String key) throws IllegalArgumentException {
+        if (context == null) {
+            throw new IllegalArgumentException("Context is null.");
         }
 
         SharedPreferences sharedPreferences = context.getSharedPreferences(storageName, Context.MODE_PRIVATE);
@@ -28,9 +48,9 @@ public class LocalStorageHelper {
         return (sharedPreferences == null ? null : sharedPreferences.getString(key, null));
     }
 
-    public void save(Context context, String key, String value) throws Exception {
+    public void save(Context context, String key, String value) throws IllegalArgumentException {
         if (context == null) {
-            throw new Exception("Context is null.");
+            throw new IllegalArgumentException("Context is null.");
         }
 
         try {
